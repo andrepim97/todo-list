fb234a048308ddc6d3cb62ac957f93dc8616304d
André Pimentel <andre.pimentel@ab-ace.pt>
2025-05-29 11:12:26 +0100

    Add initial version of TodoList and TodoItem components

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<!-- src/components/TodoItem.vue -->
+<template>
+  <li>{{ tarefa }}</li>
+</template>
+
+<script setup>
+defineProps({
+  tarefa: {
+    type: String,
+    required: true
+  }
+})
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Lista de Tarefas</h2>
+        <ul>
+            <TodoItem v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        </ul>
+        <input v-model="novaTarefa" @keyup.enter="adicionarTarefa" placeholder="Nova tarefa..." />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import TodoItem from './TodoItem.vue'
+
+const tarefas = ref([])
+const novaTarefa = ref('')
+
+function adicionarTarefa() {
+    if (novaTarefa.value.trim()) {
+        tarefas.value.push(novaTarefa.value.trim())
+        novaTarefa.value = ''
+    }
+}
+</script>
