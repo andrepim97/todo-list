9ea16f518adb78dff95da3de6d441ab9325dec04
André Pimentel <andre.pimentel@ab-ace.pt>
2025-05-30 15:41:07 +0100

    Add isolated language selector component and integrate into PageWrapper

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,30 @@
+<template>
+    <select v-model="selectedLanguage" @change="alterarIdioma" class="form-select form-select-sm w-auto">
+        <option v-for="(label, code) in idiomas" :key="code" :value="code">
+            {{ label }}
+        </option>
+    </select>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['languageChange'])
+
+const idiomas = {
+    en: 'English',
+    pt: 'Português',
+}
+
+const selectedLanguage = ref('pt')
+
+function alterarIdioma() {
+    emit('languageChange', selectedLanguage.value)
+}
+</script>
+
+<style scoped>
+select.form-select {
+    cursor: pointer;
+}
+</style>
